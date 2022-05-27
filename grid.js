bd735dffab253c55cb2c42a1b1abc1e83f04f1d2
samuel lutz <samlutz89@gmail.com>
2022-05-27 13:47:40 -0700

    grid appears dynamicly with js

--- grid.js
+++ grid.js
@@ -8,5 +8,17 @@ export default class Grid{
         gridEl.style.setProperty("--grid-size", GRID_SIZE)
         gridEl.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
         gridEl.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
+        createCellEl(gridEl)
     }
+}
+
+function createCellEl(gridEl) {
+    const cells = []
+    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+        cells.push(cell)
+        gridEl.append(cell)
+    }
+    return cells
 }
