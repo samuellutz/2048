9dfbe1f9cb8a384cdf579c1d68f3699fe9d7417e
samuel lutz <samlutz89@gmail.com>
2022-05-30 13:56:22 -0700

    tiles display with numbers

--- tile.js
+++ tile.js
@@ -2,11 +2,23 @@ export default class Tile {
     #tileEl
     #x
     #y
+    #value
+
     constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
         this.#tileEl = document.createElement('div')
         this.#tileEl.classList.add('tile')
         tileContainer.append(this.#tileEl)
         this.value = value
+    }
+
+    set value(v){
+        this.#value = v
+        this.#tileEl.textContent = v
+        const power = Math.log2(v)
+        const backgroundLightness = 100 - power * 9
+        this.#tileEl.style.setProperty('--background-lightness', `${backgroundLightness}%`)
+        this.#tileEl.style.setProperty('--text-lightness', `${backgroundLightness <= 50 ? 90 : 10}%`)
+
     }
 
     set x(value) {
